701a5e5707d073d807ad09fba4dab2fd86d9b7dc
andrew <chel@activist.com>
2016-09-16 17:22:46 +0300

    comment

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from 'angular2/core';
+import { TodoListComponent } from './todo/todo-list.component';
 
 @Component({
     selector: 'todo-app',
     templateUrl:'./app/app.component.html',
-    styleUrls: ['./app/app.component.css']
+    styleUrls: ['./app/app.component.css'],
+    directives: [TodoListComponent]
 })
 
 export class AppComponent{
@@ -16,10 +18,7 @@ export class AppComponent{
         this.todos = [];
     }
 
-    addToDo(input: HTMLInputElement){
-        let title = input.value;
-        input.value = '';
-        console.log('Добавление задачи: ' + title);
+    addToDo(title: string){
         this.todos.push(title);
     }
 }
