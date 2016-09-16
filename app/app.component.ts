f6eff64962744f0fdf1a821044f545336ce79bfc
andrew <chel@activist.com>
2016-09-16 11:17:19 +0300

    Initial commit

---
+++ app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from 'angular2/core';
+
+@Component({
+    selector: 'todo-app',
+    templateUrl:'./app/app.component.html',
+    styleUrls: ['./app/app.component.css']
+})
+
+export class AppComponent{
+
+    title: string;
+
+    constructor(){
+        this.title = 'Angular2Do';
+    }
+}
